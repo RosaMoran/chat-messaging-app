6b9643625849036a0af4f200d51b5cb407d88822
RosaMoran <rosa.moran.delgado@gmail.com>
2022-08-04 14:19:17 +0200

    adding pics and some components

---
+++ client/src/component/ChannelContainer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import  { Channel, MessageTeam } from 'stream-chat-react';
+import { ChannelInner, CreateChannel, EdithChannel } from './';
+
+const ChannelContainer = ({ isCreating, setIsCreating, isEditing, setIsEditing, createType }) => {
+    
+}
+
+const ChannelContainer = () => {
+  return (
+    <div>ChannelContainer</div>
+  )
+}
+
+export default ChannelContainer
